763a63f03b519150ee3cbbe55ced759009bb4003
jo <jo@jo-300E4A-300E5A-300E7A>
2016-11-08 22:50:53 +0100

    getForecast() - use

--- app/weatherForecast.module.js
+++ app/weatherForecast.module.js
@@ -1,3 +1,14 @@
 angular.module('weatherForecast', [
-    'forecast'
+    'forecast', 
 ]);
+
+angular.module('weatherForecast').
+  component('forecast', {
+    template: '<div>{{$ctrl.forecast}}</div>',
+    controller: ['forecastService',
+      function forecastController(forecastService) {
+        var self = this;
+        self.forecast = forecastService.getCityForecast('warsaw');
+      }
+    ]
+  });
