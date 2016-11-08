8d68c5f3481b4813cf06a80cdf83aa1af26ca56e
jo <jo@jo-300E4A-300E5A-300E7A>
2016-11-08 12:45:52 +0100

    getForecast()

--- app/forecast/forecast.service.js
+++ app/forecast/forecast.service.js
@@ -1,6 +1,36 @@
 angular.module('forecast')
     .service('forecastService', ['$http', function ($http) {
 
+        this.getCityForecast = function (city) {
+            var result;
+
+            $.ajax({
+                async: false, 
+                url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&mode=json&units=metric&cnt=7&apikey=80e6daa3a673115878aba8e33628330e',
+                dataType:'text',
+                success: function(response) {
+                    result = response;
+                }
+            });
+
+            return result;
+        }
+
+        this.getLatLonForecast = function (lat, lon) {
+            var result;
+
+            $.ajax({
+                async: false, 
+                url: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&mode=json&units=metric&cnt=7&apikey=80e6daa3a673115878aba8e33628330e',
+                dataType:'text',
+                success: function(response) {
+                    result = response;
+                }
+            });
+
+            return result;
+        }
+
         var urlBase = '/api/customers';
 
         this.getCustomers = function () {
