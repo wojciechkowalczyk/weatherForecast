351bb953f4c9c41704a42ddf9fcbb7e9a1b3fe65
jo <jo@jo-300E4A-300E5A-300E7A>
2016-11-13 16:19:08 +0100

    ord

--- app/forecast/forecast.service.js
+++ app/forecast/forecast.service.js
@@ -41,30 +41,4 @@ angular.module('forecast')
             return result;*/
             return $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&mode=json&units=metric&cnt=7&apikey=80e6daa3a673115878aba8e33628330e');
         };
-
-        var urlBase = '/api/customers';
-
-        this.getCustomers = function () {
-            return $http.get(urlBase);
-        };
-
-        this.getCustomer = function (id) {
-            return $http.get(urlBase + '/' + id);
-        };
-
-        this.insertCustomer = function (cust) {
-            return $http.post(urlBase, cust);
-        };
-
-        this.updateCustomer = function (cust) {
-            return $http.put(urlBase + '/' + cust.ID, cust)
-        };
-
-        this.deleteCustomer = function (id) {
-            return $http.delete(urlBase + '/' + id);
-        };
-
-        this.getOrders = function (id) {
-            return $http.get(urlBase + '/' + id + '/orders');
-        };
     }]);
