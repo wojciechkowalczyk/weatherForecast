4d7d8664d09e76fc30c4c29b81b4fe3ba979f820
jo <jo@jo-300E4A-300E5A-300E7A>
2016-11-18 01:36:52 +0100

    shortcut methods

--- app/forecast/forecast.service.js
+++ app/forecast/forecast.service.js
@@ -1,20 +1,13 @@
 angular.module('forecast').
-        service('forecastService', ['$http', 
+        service('forecastService', ['$http',
             function ($http) {
                 this.apiKey = '80e6daa3a673115878aba8e33628330e';
 
                 this.getCityForecast = function (city) {
                     var beforeCityQuery = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
                     var afterCityQuery = '&mode=json&units=metric&cnt=7&apikey=' + this.apiKey;
-                    
-                    return $http({
-                        url: beforeCityQuery + city + afterCityQuery,
-                        method: 'GET',
-                        headers: {
-                            "content-type": "application/json",
-                            "Accept": "application/json"
-                        }
-                    }).then(function (result) {
+
+                    return $http.get(beforeCityQuery + city + afterCityQuery).then(function (result) {
                         return result.data;
                     });
                 };
@@ -24,14 +17,7 @@ angular.module('forecast').
                     var innerQuery = '&lon=';
                     var afterLonQuery = '&mode=json&units=metric&cnt=7&apikey=' + this.apiKey;
 
-                    return $http({
-                        url: beforeLatQuery + lat + innerQuery + lon + afterLonQuery,
-                        method: 'GET',
-                        headers: {
-                            "content-type": "application/json",
-                            "Accept": "text/plain"
-                        }
-                    }).then(function (result) {
+                    return $http.get(beforeLatQuery + lat + innerQuery + lon + afterLonQuery).then(function (result) {
                         return result.data;
                     });
                 };
